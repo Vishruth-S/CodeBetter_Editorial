9598cb41cd7a39cc32132ef5acb3abeefbbb8646
Vishruth S <vs.vishruth.s@gmail.com>
2021-05-19 01:32:25 +0530

    added solutions

---
+++ Debug/7_ConvertFtoC.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int F;
+    cin >> F;
+    int C;
+    C = (F - 32) * 5 / 9;
+    cout << C;
+    return 0;
+}
